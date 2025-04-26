af5d610267b430938e8122f80324cb6db05f0f58
Faisal Malik <faisalmalik@Faisals-MacBook-Pro.local>
2025-04-27 01:16:07 +0500

    ConversationId is added in bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -393,7 +393,7 @@ getBookings = async (req, res) => {
         dropTime: fmtTime(b.returnTime),
         pickupOTP: b.pickupOTP,
         dropOffOTP: b.dropOffOTP,
-        conversationId: b.Conversation.id || null,
+        conversationId: b?.Conversation?.id,
         car: {
           id: car.id,
           name: car.name,
